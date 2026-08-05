7bdf0e50041ea763d14b06d9419c8728a1309dea
JHolley7 <jariah.holley05@gmail.com>
2026-08-05 23:56:39 +0000

    added copies

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,9 @@ const clone = original.cloneNode(true);
 container.appendChild(clone);
 
 clone.classList.add('echo');
+
+for (let i = 0; i < 20; i++) {
+  const clone = original.cloneNode(true);
+  clone.classList.add('echo');
+  container.appendChild(clone);
+}
